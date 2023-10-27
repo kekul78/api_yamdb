3195338d2d45aaf1a2839d31a837755c35a774f0
kekul <S-HENTAY-S@yandex.ru>
2023-10-27 12:31:59 +0300

    Added fixed

---
+++ api_yamdb/reviews/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from reviews.models import Comment, Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    review = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Review
